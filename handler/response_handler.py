a1614bd2164d518debaddb337e19c75961bd4939
20jun01 <10junjun20@gmail.com>
2023-03-05 03:08:02 +0900

    🐛 format...

--- handler/response_handler.py
+++ handler/response_handler.py
@@ -1,4 +1,6 @@
-import json, requests, os
+import json
+import requests
+import os
 
 # 環境変数からaccess tokenを読み込む*1
 BOT_ACCESS_TOKEN = os.environ["BOT_ACCESS_TOKEN"]
@@ -6,15 +8,18 @@ TRAQ_API_URL = os.environ["TRAQ_URL"]
 BOT_ID = os.environ["BOT_ID"]
 
 # textをchannel_idに投稿する関数*3
+
+
 def post_to_traq(text: str, channel_id: str) -> None:
-    url: str = f"TRAQ_API_URL/channels/{channel_id}/messages"
+    url: str = f"{TRAQ_API_URL}/channels/{channel_id}/messages"
     data: dict = {
         "content": text,
         "embed": True
     }
     headers: dict = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {BOT_ACCESS_TOKEN}"  # BOTからのtraQへのリクエストにはaccess tokenが必要*1
+        # BOTからのtraQへのリクエストにはaccess tokenが必要*1
+        "Authorization": f"Bearer {BOT_ACCESS_TOKEN}"
     }
     r: requests.Response = requests.post(
         url, data=json.dumps(data), headers=headers)
@@ -22,15 +27,17 @@ def post_to_traq(text: str, channel_id: str) -> None:
     print(response_body)
 
 # channel_idに参加する関数*4
+
+
 def join_channel(channel_id: str) -> None:
-    url: str = f"TRAQ_API_URL/bots/{BOT_ID}/actions/join"
+    url: str = f"{TRAQ_API_URL}/bots/{BOT_ID}/actions/join"
     data: dict = {
         "channelId": channel_id
     }
 
     headers: dict = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {BOT_ACCESS_TOKEN}" 
+        "Authorization": f"Bearer {BOT_ACCESS_TOKEN}"
     }
 
     r: requests.Response = requests.post(
@@ -39,15 +46,17 @@ def join_channel(channel_id: str) -> None:
     print(response_body)
 
 # channel_idから退出する関数*5
+
+
 def leave_channel(channel_id: str) -> None:
-    url: str = f"TRAQ_API_URL/bots/{BOT_ID}/actions/leave"
+    url: str = f"{TRAQ_API_URL}/bots/{BOT_ID}/actions/leave"
     data: dict = {
         "channelId": channel_id
     }
 
     headers: dict = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {BOT_ACCESS_TOKEN}" 
+        "Authorization": f"Bearer {BOT_ACCESS_TOKEN}"
     }
 
     r: requests.Response = requests.post(
