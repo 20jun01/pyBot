f39bab82fe088e72123964014a91a34d34047ecb
20jun01 <10junjun20@gmail.com>
2023-03-05 01:07:15 +0900

    🐛 module is not callable

--- handler/verification_handler.py
+++ handler/verification_handler.py
@@ -1,7 +1,7 @@
 import os
 
 verificationToken = os.getenv("VERIFICATION_TOKEN")
-def verificatioN_handler(headers) -> bool:
+def verification_handler(headers) -> bool:
     try:
         event = headers["X-TRAQ-BOT-EVENT"]
         reqID = headers["X-TRAQ-BOT-REQUEST-ID"]
--- main.py
+++ main.py
@@ -8,22 +8,26 @@ with open("system_setting.txt", "r") as f:
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root(request: Request):
-    if verification_handler(request.headers):
+    if verification_handler.verification_handler(request.headers):
         return Response(status_code=204)
     else:
         return HTTPException(status_code=403, detail="Forbidden")
 
+
 @app.post("/talk", response_model=TalkResponse)
 async def generate_talk(request: TalkRequest):
     global past_messages
-    new_message, past_messages = talk_handler.talk_handler([], request.message, system_settings)
+    new_message, past_messages = talk_handler.talk_handler(
+        [], request.message, system_settings)
     return TalkResponse(message=":@BOT_urturn225: < " + new_message)
 
 
 @app.post("/talk/cont", response_model=TalkResponse)
 async def generate_talk(request: TalkRequest):
     global past_messages
-    new_message, messages = talk_handler.talk_handler(past_messages, request.message, system_settings)
+    new_message, messages = talk_handler.talk_handler(
+        past_messages, request.message, system_settings)
     return TalkResponse(message=":@BOT_urturn225: < " + new_message)
