078dc5de6aac4ab7a5aad82701ba0b52c4bf8b8d
20jun01 <10junjun20@gmail.com>
2023-03-05 02:19:42 +0900

    🐛 fix args

--- handler/talk_handler.py
+++ handler/talk_handler.py
@@ -7,15 +7,15 @@ past_message = []
 
 def generate_talk(message: str) -> str:
     global past_messages
-    new_message, message = talk_handler([], message, system_settings)
+    new_message, message = talk_handler([], message)
     past_messages = message
-    return ":@BOT_urturn225: < " + new_message
+    return new_message
 
 def generate_talk_cont(message: str) -> str:
     global past_messages
-    new_message, _ = talk_handler(past_messages, message, system_settings)
+    new_message, _ = talk_handler(past_messages, message)
     past_messages = message
-    return ":@BOT_urturn225: < " + new_message
+    return new_message
 
 def talk_handler(past_messages: str, message: str):
     new_message, messages = open_ai.completion(message, system_settings, past_messages)
