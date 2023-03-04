f025a4f66cbed8004cfdbfd989d07f836ee163e3
20jun01 <10junjun20@gmail.com>
2023-03-05 02:24:36 +0900

    🐛 post to traq

--- handler/message_handler.py
+++ handler/message_handler.py
@@ -10,11 +10,11 @@ def message_created_response(body: dict) -> Response:
 
     if message_sent.startswith("/cont"):
         message="@" + body["message"]["user"]["name"] + " もっと話したいんだね:okk:\n" + talk_handler.generate_talk_cont(message_sent)
-        response_handler.send_message(message, body["message"]["channelId"])
+        response_handler.post_to_traq(message, body["message"]["channelId"])
         return Response(status_code=204)
 
     else:
         message=body["message"]["user"]["displayName"] + "さん :oisu-1::oisu-2::oisu-3::oisu-4yoko:\n" + talk_handler.generate_talk(message_sent)
-        response_handler.send_message(message, body["message"]["channelId"])
+        response_handler.post_to_traq(message, body["message"]["channelId"])
         return Response(status_code=204)
     
