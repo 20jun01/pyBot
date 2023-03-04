7501955d2833a00dd5bbfe7eae61401701904b58
20jun01 <10junjun20@gmail.com>
2023-03-05 02:23:34 +0900

    🐛

--- handler/talk_handler.py
+++ handler/talk_handler.py
@@ -19,4 +19,4 @@ def generate_talk_cont(message: str) -> str:
 
 def talk_handler(past_messages: str, message: str):
     new_message, messages = open_ai.completion(message, system_settings, past_messages)
-    return tuple(new_message, messages)
+    return new_message, messages
