670e5da37351d6dbae5ec8fb356311233bcbc694
20jun01 <10junjun20@gmail.com>
2023-03-05 02:59:34 +0900

    ✨ join and leave

--- handler/message_handler.py
+++ handler/message_handler.py
@@ -8,6 +8,27 @@ def message_created_response(body: dict) -> Response:
 
     message_sent = body["message"]["plainText"]
 
+    if message_sent.startswith("/help"):
+        message = ""
+        message += "コマンド一覧\n"
+        message += "/help: このメッセージを表示します\n"
+        message += "/cont: トークを続けます\n"
+        message += "/join: チャンネルに参加します\n"
+        message += "/leave: チャンネルから退出します\n"
+        message += "その他: お話しします\n"
+        response_handler.post_to_traq(message, body["message"]["channelId"])
+        return Response(status_code=204)
+
+    elif message_sent.startswith("@BOT_urturn_Talker /join"):
+        response_handler.join_channel(body["message"]["channelId"], )
+        response_handler.post_to_traq(":oisu-1::oisu-2::oisu-3::oisu-4yoko:", body["message"]["channelId"])
+        return Response(status_code=204)
+
+    elif message_sent.startswith("/leave"):
+        response_handler.leave_channel(body["message"]["channelId"])
+        response_handler.post_to_traq(":snailchan_jito.ex-large.ex-large:", body["message"]["channelId"])
+        return Response(status_code=204)
+
     if message_sent.startswith("/cont"):
         message="@" + body["message"]["user"]["name"] + " もっと話したいんだね:okk:\n" + talk_handler.generate_talk_cont(message_sent)
         response_handler.post_to_traq(message, body["message"]["channelId"])
--- handler/response_handler.py
+++ handler/response_handler.py
@@ -3,10 +3,11 @@ import json, requests, os
 # 環境変数からaccess tokenを読み込む*1
 BOT_ACCESS_TOKEN = os.environ["BOT_ACCESS_TOKEN"]
 TRAQ_API_URL = os.environ["TRAQ_URL"]
+BOT_ID = os.environ["BOT_ID"]
 
 # textをchannel_idに投稿する関数*3
 def post_to_traq(text: str, channel_id: str) -> None:
-    url: str = TRAQ_API_URL + f"/channels/{channel_id}/messages"
+    url: str = f"TRAQ_API_URL/channels/{channel_id}/messages"
     data: dict = {
         "content": text,
         "embed": True
@@ -15,6 +16,40 @@ def post_to_traq(text: str, channel_id: str) -> None:
         "Content-Type": "application/json",
         "Authorization": f"Bearer {BOT_ACCESS_TOKEN}"  # BOTからのtraQへのリクエストにはaccess tokenが必要*1
     }
+    r: requests.Response = requests.post(
+        url, data=json.dumps(data), headers=headers)
+    response_body = r.json()
+    print(response_body)
+
+# channel_idに参加する関数*4
+def join_channel(channel_id: str) -> None:
+    url: str = f"TRAQ_API_URL/bots/{BOT_ID}/actions/join"
+    data: dict = {
+        "channelId": channel_id
+    }
+
+    headers: dict = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {BOT_ACCESS_TOKEN}" 
+    }
+
+    r: requests.Response = requests.post(
+        url, data=json.dumps(data), headers=headers)
+    response_body = r.json()
+    print(response_body)
+
+# channel_idから退出する関数*5
+def leave_channel(channel_id: str) -> None:
+    url: str = f"TRAQ_API_URL/bots/{BOT_ID}/actions/leave"
+    data: dict = {
+        "channelId": channel_id
+    }
+
+    headers: dict = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {BOT_ACCESS_TOKEN}" 
+    }
+
     r: requests.Response = requests.post(
         url, data=json.dumps(data), headers=headers)
     response_body = r.json()
