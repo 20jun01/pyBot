9fcf5771d41feec2d400403e491da11e366e13da
20jun01 <10junjun20@gmail.com>
2023-03-05 00:48:51 +0900

    🚚 move functions

--- handler/talk_handler.py
+++ handler/talk_handler.py
@@ -1,4 +1,4 @@
-from ..functions import completion
+from functions import completion
 
 def talk_handler(past_messages: str, message: str, system_settings: str) -> tuple(str, list[dict(str, str)]):
     new_message, messages = completion(message, system_settings, past_messages)
