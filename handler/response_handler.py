527490c7dbecafc6e8e7f36254b7e278a9dbaabf
20jun01 <10junjun20@gmail.com>
2023-03-05 02:10:28 +0900

    ⚡️ add environ

--- handler/response_handler.py
+++ handler/response_handler.py
@@ -2,10 +2,11 @@ import json, requests, os
 
 # 環境変数からaccess tokenを読み込む*1
 BOT_ACCESS_TOKEN = os.environ["BOT_ACCESS_TOKEN"]
+TRAQ_API_URL = os.environ["TRAQ_URL"]
 
 # textをchannel_idに投稿する関数*3
 def post_to_traq(text: str, channel_id: str) -> None:
-    url: str = f"https://q.trap.jp/api/v3/channels/{channel_id}/messages"
+    url: str = TRAQ_API_URL + f"/channels/{channel_id}/messages"
     data: dict = {
         "content": text,
         "embed": True
