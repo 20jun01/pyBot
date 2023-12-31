1011b62cbe2bae5b49a2cdc70fcc04e6fffd836a
20jun01 <10junjun20@gmail.com>
2023-12-31 23:25:06 +0900

    🩹 debug

--- src/handler/verification_handler.py
+++ src/handler/verification_handler.py
@@ -6,10 +6,13 @@ verificationToken = os.getenv("VERIFICATION_TOKEN")
 
 # TODO: middleware的に作る
 def verification_handler(headers) -> str:
+    print(headers)
     try:
         event = headers["X-TRAQ-BOT-EVENT"]
         reqID = headers["X-TRAQ-BOT-REQUEST-ID"]
         token = headers["X-TRAQ-BOT-TOKEN"]
+        if event =="PING":
+            return event
     except KeyError:
         raise HTTPException(status_code=403, detail="Forbidden")
 
